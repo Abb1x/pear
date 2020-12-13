a30fca496dbff3c9b16e7e6f62ec455fafef7405
Ahmed <ahmed.hussain2080@gmail.com>
2020-12-13 08:02:35 +0500

    updated render
    cleaner code

--- src/engine/renderer.cpp
+++ src/engine/renderer.cpp
@@ -1,54 +1,76 @@
-
 #include "../inc/renderer.hpp"
 #include <iostream>
+
 void Renderer::createWindow(const char* title, int w, int h)
+
 {
 
       window = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED, w, h, SDL_WINDOW_SHOWN);
 
 }
+
 void Renderer::createRenderer()
+
 {
+      
   renderer = SDL_CreateRenderer(window, -1, 0);
 
 }
+
 void Renderer::setRendererColor(int r,int g, int b, int a)
+
 {
+      
 
   SDL_SetRenderDrawColor(renderer,r,g,b,a);
 
 }
 
 void Renderer::clearRenderer()
+
 {
+      
 
   SDL_RenderClear(renderer);
 
 }
+
 void Renderer::renderPresent()
+
 {
+      
   SDL_RenderPresent(renderer);
 }
+
 void Renderer::destroyRenderer()
+
 {
+      
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
 
 }
+
 SDL_Texture* Renderer::loadTexture(const char* path)
 {
+      
   SDL_Texture* texture = NULL;
   texture = IMG_LoadTexture(renderer,path);
   if(texture == NULL)
+        
     {
 
       std::cout<<"Pear failed to load a texture, error: "<<SDL_GetError()<<std::endl;
     }
+      
   return texture;
 }
+
 void Renderer::renderSprite(SDL_Texture* tex,int x, int y, float scale, double angle)
+      
 {
+      
   int w, h;
   SDL_QueryTexture(tex, NULL, NULL, &w, &h);
   texr.x = x; texr.y = y; texr.w = w * scale  ; texr.h = h * scale; 
@@ -57,6 +79,7 @@ void Renderer::renderSprite(SDL_Texture* tex,int x, int y, float scale, double a
 }
 
 void Renderer::renderFont(SDL_Color color, const char* text, int size,const char* path, int x ,int y)
+      
 {
   
   TTF_Font * font = TTF_OpenFont(path,size);
@@ -69,10 +92,13 @@ void Renderer::renderFont(SDL_Color color, const char* text, int size,const char
   SDL_RenderCopy(renderer, texture, NULL, &dstrect);
   SDL_DestroyTexture(texture);
   SDL_FreeSurface(surface);
+      
 }
 
 void Renderer::testAnimation()
+      
 {
+      
   texr.x += 1;
 
 }
